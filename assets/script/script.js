a7a7e2039f1cadfc6a3d876786c856bc631a63b8
Eric Sun <ericmsun@gmail.com>
2021-08-07 16:44:20 -0400

    final save

--- assets/script/script.js
+++ assets/script/script.js
@@ -3,36 +3,39 @@ var generateBtn = document.querySelector("#generate");
 
 // Array of special characters/ numbers/ upper/ lowercase
 var specialChar = ['!', '@', '#', '$', '%', '&', '*'];
-console.log(specialChar);
+// console.log(specialChar);
 var numberChar = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-console.log(numberChar)
+// console.log(numberChar)
 var lowerStr = 'a b c d e f g h i j k l m n o p q r s t u v w x y z';
 var lowerCase = lowerStr.split(' ');
-console.log(lowerCase);
+// console.log(lowerCase);
 var upperStr = 'A B C D E F G H I J K L M N O P Q R S T U V W X Y Z';
 var upperCase = upperStr.split(' ');
-console.log(upperCase);
+// console.log(upperCase);
+
+ // Write password to the #password input
+ 
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
 // function to prompts user of password options
-function generatePassword() {
   function getUserOptions() {
     //  Created a variable to store password length from userInput
     var passLength = prompt('Enter Password Length: Choose between 8 - 128 charcters');
-    console.log(passLength);
+    // console.log(passLength);
     // parseInt() to make string to array
     var passLengthInt = parseInt(passLength)
-    console.log(passLengthInt);
+    // console.log(passLengthInt);
 
 
     // create a conditional statement to check if the lenght is an actual number 8 Eight
     // create a conditional to check if PW lenght is atleast 8 char long
     // create a conditional to check if PW lenght is lower than 128 chars
 
-    if (isNaN(passLengthInt == true)) {
+    if (isNaN(passLengthInt)) {
       passLengthInt
-      console.log(passLengthInt)
-    } else if (isNaN(passLengthInt !== true)) {
-      alert('Must use numbers!');
+      alert("Must enter NUMBER characters")
       return
     } else if (passLengthInt < 8) {
       alert('Password must be minimum 8 characters');
@@ -43,64 +46,96 @@ function generatePassword() {
     };
 
 
-
-
     //storing user input for character options choices
     if (confirm('Do you want to use special characters?') == true) {
       specialChar
-      console.log(specialChar)
+      // console.log(specialChar)
     } else {
-      !specialChar
-      console.log(!specialChar)
+      specialChar = [];
+      // console.log(specialChar)
     };
 
     if (confirm('Do you want to use numbers?') == true) {
       numberChar
-      console.log(numberChar)
+      // console.log(numberChar)
     } else {
-      !numberChar
-      console.log(!numberChar)
+      numberChar = []
+      // console.log(numberChar)
     };
 
     if (confirm('Do you want to use lowercase letters?') == true) {
       lowerCase
-      console.log(lowerCase)
+      // console.log(lowerCase)
     } else {
-      !lowerCase
-      console.log(!lowerCase)
+      lowerCase = []
+      // console.log(lowerCase)
     };
 
     if (confirm('Do you want to use uppercase letters?') == true) {
       upperCase
-      console.log(upperCase)
+      // console.log(upperCase)
     } else {
-      !upperCase
-      console.log(!upperCase)
+      upperCase = []
+      // console.log(upperCase)
     };
-    return getUserOptions;
+  // console.log([specialChar, numberChar, lowerCase, upperCase]);
+
+  // Create a var to store user input
+  var userInput = {
+    passLength: passLengthInt,
+    specialChar,
+    numberChar: numberChar,
+    lowerCase: lowerCase,
+    upperCase: upperCase,
+  }
+  return userInput
+  
+
+
+
   };
 
-};
-    //Conditional statement to check to user input
+  function randomElement (randomArray) {
+    return randomArray[Math.floor(Math.random()*randomArray.length)];
+  }
+  
 
-    // if(passLengthInt == 0 || !numberChar || !specialChar || !lowerCase || !upperCase) {
-    //   return('empty input')
-    // }
+  function writePassword() {
+    
+    var userOptions = getUserOptions();
+    var results = []
+    var userPassChars = []
+    var guarChar = []
 
-    // }
-
-
-
-
-    // Write password to the #password input
-    function writePassword() {
-      getUserOptions();
-      var password = generatePassword();
-      var passwordText = document.querySelector("#password");
-
-      passwordText.value = password;
-
+    if(userOptions.specialChar) {
+      userPassChars = userPassChars.concat(userOptions.specialChar)
+      guarChar.push(randomElement(specialChar));
     }
+    if(userOptions.numberChar) {
+      userPassChars = userPassChars.concat(userOptions.numberChar)
+      guarChar.push(randomElement(numberChar));
+    }
+    if(userOptions.lowerCase) {
+      userPassChar = userPassChars.concat(userOptions.lowerCase)
+      guarChar.push(randomElement(lowerCase));
+    }
+    if(userOptions.upperCase) {
+      userPassChars = userPassChars.concat(userOptions.upperCase)
+      guarChar.push(randomElement(upperCase));
+    }
+    // console.log(userPassChars);
+    // console.log(guarChar);
+    
+    //var password = generatePassword();
+    // var passwordText = document.querySelector("#password");
+  
+    // passwordText.value = password;
+    for(i = 0; i < userOptions.passLength; i++) {
+      var randomCharacter = randomElement(userPassChars)
+      results += randomCharacter
 
-    // Add event listener to generate button
-    generateBtn.addEventListener("click", writePassword);
+    };
+    alert(results)
+    return results
+
+  };
