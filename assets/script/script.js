f21de7204abbda83da969ee9b488ec4523144967
Eric Sun <ericmsun@gmail.com>
2021-08-07 13:59:38 -0400

    loading last saved point

--- assets/script/script.js
+++ assets/script/script.js
@@ -14,111 +14,93 @@ var upperCase = upperStr.split(' ');
 console.log(upperCase);
 
 // function to prompts user of password options
-function getUserOptions() {
-  //  Created a variable to store password length from userInput
-  var passLength = prompt('Enter Password Length: Choose between 8 - 128 charcters');
-  console.log(passLength);
+function generatePassword() {
+  function getUserOptions() {
+    //  Created a variable to store password length from userInput
+    var passLength = prompt('Enter Password Length: Choose between 8 - 128 charcters');
+    console.log(passLength);
+    // parseInt() to make string to array
+    var passLengthInt = parseInt(passLength)
+    console.log(passLengthInt);
 
-  // parseInt() to make string to array
-  var passLengthInt = parseInt(passLength)
-  console.log(passLengthInt);
 
+    // create a conditional statement to check if the lenght is an actual number 8 Eight
+    // create a conditional to check if PW lenght is atleast 8 char long
+    // create a conditional to check if PW lenght is lower than 128 chars
 
-// create a conditional statement to check if the lenght is an actual number 8 Eight
-// create a conditional to check if PW lenght is atleast 8 char long
-// create a conditional to check if PW lenght is lower than 128 chars
-
-  if (isNaN(passLengthInt == true)) {
+    if (isNaN(passLengthInt == true)) {
       passLengthInt
       console.log(passLengthInt)
-  } else if (isNaN(passLengthInt !== true)) {
-    alert('Must use numbers!');
-    return
-  } else if (passLengthInt < 8) {
-    alert('Password must be minimum 8 characters');
-    return
-  } else if (passLengthInt > 128) {
-    alert('Password must be a maximum of 128 characters');
-    return
+    } else if (isNaN(passLengthInt !== true)) {
+      alert('Must use numbers!');
+      return
+    } else if (passLengthInt < 8) {
+      alert('Password must be minimum 8 characters');
+      return
+    } else if (passLengthInt > 128) {
+      alert('Password must be a maximum of 128 characters');
+      return
+    };
+
+
+
+
+    //storing user input for character options choices
+    if (confirm('Do you want to use special characters?') == true) {
+      specialChar
+      console.log(specialChar)
+    } else {
+      !specialChar
+      console.log(!specialChar)
+    };
+
+    if (confirm('Do you want to use numbers?') == true) {
+      numberChar
+      console.log(numberChar)
+    } else {
+      !numberChar
+      console.log(!numberChar)
+    };
+
+    if (confirm('Do you want to use lowercase letters?') == true) {
+      lowerCase
+      console.log(lowerCase)
+    } else {
+      !lowerCase
+      console.log(!lowerCase)
+    };
+
+    if (confirm('Do you want to use uppercase letters?') == true) {
+      upperCase
+      console.log(upperCase)
+    } else {
+      !upperCase
+      console.log(!upperCase)
+    };
+    return getUserOptions;
   };
 
-  //storing user input for character options choices
-  if (confirm('Do you want to use special characters?') == true) {
-    specialChar
-    console.log(specialChar)
-  } else {
-    !specialChar
-    console.log(!specialChar)
-  };
+};
+    //Conditional statement to check to user input
 
-  if (confirm('Do you want to use numbers?') == true) {
-    numberChar
-    console.log(numberChar)
-  } else {
-    !numberChar
-    console.log(!numberChar)
-  };
+    // if(passLengthInt == 0 || !numberChar || !specialChar || !lowerCase || !upperCase) {
+    //   return('empty input')
+    // }
 
-  if (confirm('Do you want to use lowercase letters?') == true) {
-    lowerCase
-    console.log(lowerCase)
-  } else {
-    !lowerCase
-    console.log(!lowerCase)
-  };
-
-  if (confirm('Do you want to use uppercase letters?') == true) {
-    upperCase
-    console.log(upperCase)
-  } else {
-    !upperCase
-    console.log(!upperCase)
-  };
-
-
-
-//Conditional statement to check to user input
-
-
-
-  if(passLengthInt == 0 || !numberChar || !specialChar || !lowerCase || !upperCase) {
-    return('empty input')
-  } 
+    // }
 
 
 
 
-// if(isNaN(passLengthInt) || passLengthInt < 8 || passLengthInt > 128) {
-//   alert('Must choose password 8 - 128 characters long')
-//   console.log()
-// } else if (!lowerCase || !upperCase || !numberChar || !specialChar) {
-//   alert('Must choose atleast 1 type of characters')
-//   console.log()
-// } else {
-//   alert('Input recieved!')
-//   console.log()
-// };
+    // Write password to the #password input
+    function writePassword() {
+      getUserOptions();
+      var password = generatePassword();
+      var passwordText = document.querySelector("#password");
 
+      passwordText.value = password;
 
+    }
 
-
-
-
-
-}
-
-
-
-
-// Write password to the #password input
-function writePassword() {
-  getUserOptions();
-  // var password = generatePassword();
-  // var passwordText = document.querySelector("#password");
-
-  // passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+    // Add event listener to generate button
+    generateBtn.addEventListener("click", writePassword);
